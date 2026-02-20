e62662aa65bcf9b03f1e38f21cd2ff9975649282
susan.huang <susan.huang@appier.com>
2026-02-21 01:06:46 +0800

    Add 04 module: dbt

---
+++ 04-Analytics-Engineering/load_data_fhv.py
@@ -0,0 +1,74 @@
+import os
+import json
+from dotenv import load_dotenv
+import pandas as pd
+from google.cloud import bigquery, storage
+from google.oauth2 import service_account
+from pathlib import Path
+import requests
+
+load_dotenv()
+
+# Change this to your bucket name
+BUCKET_NAME = os.getenv("GCP_BUCKET_NAME")
+PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+DATASET_ID = os.getenv("BIGQUERY_DATASET_ID")
+
+BASE_URL = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
+
+def download_and_upload(taxi_type = 'fhv'):
+    data_dir = Path("data") / taxi_type
+    data_dir.mkdir(exist_ok=True, parents=True)
+
+    for year in [2019]:
+        for month in range(1, 13):
+            csv_gz_filename = f"{taxi_type}_tripdata_{year}-{month:02d}.csv.gz"
+            csv_gz_filepath = data_dir / csv_gz_filename
+
+            # Download file
+            print(f"Downloading {csv_gz_filename}...")
+            response = requests.get(f"{BASE_URL}/{taxi_type}/{csv_gz_filename}", stream=True)
+            
+            if response.status_code != 200:
+                print(f"File {csv_gz_filename} not found, skipping.")
+                continue
+
+            with open(csv_gz_filepath, 'wb') as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+
+            # Upload parquet to bigquery
+            try:
+                upload_to_bigquery(csv_gz_filepath, taxi_type)
+            except Exception as e:
+                print(f"Failed to upload {csv_gz_filename}: {e}")
+
+            # Delete the downloaded file to save space
+            csv_gz_filepath.unlink()
+
+def upload_to_bigquery(csv_gz_path, taxi_type):
+    client = bigquery.Client(project=PROJECT_ID)
+    table_id = f"{PROJECT_ID}.{DATASET_ID}.{taxi_type}_tripdata_new"
+    print(f"Reading {csv_gz_path.name} with Pandas...")
+    df = pd.read_csv(csv_gz_path, compression='gzip', low_memory=False)
+
+    if taxi_type == 'fhv':
+        df['pickup_datetime'] = pd.to_datetime(df['pickup_datetime'])
+        df['dropOff_datetime'] = pd.to_datetime(df['dropOff_datetime'])
+
+    job_config = bigquery.LoadJobConfig(
+        write_disposition="WRITE_APPEND", 
+        autodetect=True,
+    )
+
+    print(f"Uploading to BigQuery: {table_id}...")
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+    job.result()
+    print(f"Successfully loaded {csv_gz_path.name}")
+    
+
+
+
+if __name__ == "__main__":
+    for taxi_type in ["fhv"]:
+        download_and_upload(taxi_type)
---
+++ 04-Analytics-Engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,29 @@
+with source as (
+    select * from {{ source('raw', 'fhv_tripdata') }}
+),
+
+renamed as (
+    select
+        -- identifiers
+        cast(dispatching_base_num as string) as dispatching_base_num,
+        cast(pulocationid as integer) as pickup_location_id,
+        cast(dolocationid as integer) as dropoff_location_id,
+
+        -- timestamps
+        cast(pickup_datetime as timestamp) as pickup_datetime,  -- lpep = Licensed Passenger Enhancement Program (green taxis)
+        cast(dropoff_datetime as timestamp) as dropoff_datetime,
+
+        -- trip info
+        cast(sr_flag as integer) as sr_flag,
+        cast(affiliated_base_number as string) as Affiliated_base_number
+    from source
+    -- Filter out records with null vendor_id (data quality requirement)
+    where dispatching_base_num is not null
+)
+
+select * from renamed
+
+-- Sample records for dev environment using deterministic date filter
+{% if target.name == 'dev' %}
+where pickup_datetime >= '2019-01-01' and pickup_datetime < '2019-02-01'
+{% endif %}
